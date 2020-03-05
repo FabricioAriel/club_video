81842be1293b2bc6cd6f654f82b26b544960715d
Rodrigo Saravia <rodrigosaraviaalquez@gmail.com>
2020-03-05 19:01:11 -0400

    PanelFondo2

--- club_video/src/Vistas/PanelFondo.java
+++ club_video/src/Vistas/PanelFondo.java
@@ -1,4 +1,3 @@
-//holaaa
 package Vistas;
 
 import java.awt.Dimension;
@@ -6,3 +5,20 @@ import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+
+public class PanelFondo extends JPanel{
+    ImageIcon imagen;
+    public PanelFondo(String direc_fondo) {
+        super();
+        imagen=new ImageIcon(getClass().getResource(direc_fondo));
+    }
+    protected void paintComponent (Graphics g)
+    {
+        Dimension d=this.getSize();
+        g.drawImage(imagen.getImage(),0,0,d.width,d.height,null);
+        this.setOpaque(false);
+        super.paintComponent(g);
+    }
+}
+
+
